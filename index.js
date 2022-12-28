88dc4f3033eb5fd65ba069c0d3c981a51860ba82
Abhishek <abhishekydv2289@gmail.com>
2022-12-28 22:05:32 +0530

    resolving cors issue

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const connectDB = require('./src/config/db.config');
 const useAPI = require('./src/routes/route');
+const cors = require('cors')
 const app= express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(cors());
 app.get('/',(req,res)=>res.json({message:"connected"}))
 useAPI(app);
 connectDB();
