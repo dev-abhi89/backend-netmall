e80a5a86b3967062af6c1cea70e0f9084914059d
Abhishek <abhishekydv2289@gmail.com>
2022-12-28 14:43:53 +0530

    minor fix

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app= express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.get('/',(req,res)=>res.json({message:"connected"}))
 useAPI(app);
 connectDB();
 
